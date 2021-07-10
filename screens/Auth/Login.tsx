0d43900dac1d7d99d975e141ea0d22d65eba720f
tatendabakozw <tatendabakozw@gmail.com>
2021-07-11 01:15:55 +0200

    login page done

--- screens/Auth/Login.tsx
+++ screens/Auth/Login.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar'
 import React, { useLayoutEffect } from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
 import Login_Pic from '../../assets/icons/Login_Pic'
+import {Input} from 'react-native-elements'
 
 interface Props {
     navigation :any
@@ -23,9 +24,17 @@ const Login = ({navigation}:Props) => {
                     <Login_Pic height={250} width={250}/>
                 </View>
                 <View style={styles.login__bottom}>
-                    <Text>
-                        bottom
+                    <Text style={styles.login__heading}>
+                        Login
                     </Text>
+                    <Input placeholder="email" containerStyle={styles.login__input} />
+                    <Input placeholder="password" containerStyle={styles.login__input} />
+                    <TouchableOpacity style={styles.login__button}>
+                        <Text style={{color: "white", fontSize: 15, textAlign: 'center', fontWeight: 'bold'}}>LOGIN</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Text style={{color: "#5B61B9", fontSize: 15, textAlign: 'center', fontWeight: 'bold'}}>SIGN UP</Text>
+                    </TouchableOpacity>
                 </View>
             </KeyboardAvoidingView>
         </View>
@@ -51,7 +60,24 @@ const styles = StyleSheet.create({
         height: "65%",
         borderTopRightRadius: 50,
         borderTopLeftRadius: 50,
-        padding: 20
-    }
+        padding: 30,
+        alignItems: 'center'
+    },
+    login__heading:{
+        fontSize: 40,
+        fontFamily: 'sans-serif',
+        marginBottom: 15
+    },
+    login__input:{
+        marginBottom: 15
+    },
+    login__button:{
+        backgroundColor: "#5B61B9",
+        width: '100%',
+        padding: 15,
+        borderRadius: 50,
+        marginBottom: 20
+    },
+    loginSign__button:{}
     
 })
---
+++ screens/Auth/Register.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+interface Props {
+    navigation :any
+}
+
+const Register = ({navigation}:Props) => {
+    return (
+        <View>
+            <Text>
+                register page
+            </Text>
+        </View>
+    )
+}
+
+export default Register
+
+const styles = StyleSheet.create({})
