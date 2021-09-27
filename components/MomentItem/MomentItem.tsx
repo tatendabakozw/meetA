c2e34a32e7223c2c1d4e61f7bb73be6130ee3e13
tatendabakozw <tatendabakozw@gmail.com>
2021-09-28 01:06:46 +0200

    refactor: home moment item

--- components/MomentItem/MomentItem.tsx
+++ components/MomentItem/MomentItem.tsx
@@ -6,7 +6,7 @@ const MomentItem = () => {
     return (
         <View style={[tw`border-2 overflow-hidden border-blue-700 mr-2`, { borderRadius: 50 }]}>
             <View style={[tw`border overflow-hidden h-16 w-16 border-white`, { borderRadius: 50 }]}>
-                <Image source={require('../../assets/imgs/man.png')} blurRadius={10} style={[tw`h-16 w-16`, { borderRadius: 50 }]} resizeMode="cover" />
+                <Image source={require('../../assets/imgs/placeholder1.png')} blurRadius={10} style={[tw`h-16 w-16`, { borderRadius: 50 }]} resizeMode="cover" />
             </View>
         </View>
     )
--- components/Moments/Moments.tsx
+++ components/Moments/Moments.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
-import { AntDesign, MaterialCommunityIcons } from '@expo/vector-icons';
+import { ScrollView, View } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
 import MomentItem from '../MomentItem/MomentItem';
 import tw from 'tailwind-react-native-classnames';
 
 const Moments = () => {
     return (
         <View>
-            <ScrollView horizontal contentContainerStyle={tw`my-4`}>
+            <ScrollView horizontal contentContainerStyle={tw`mt-4 mb-2`}>
                 <View style={[tw`overflow-hidden mr-4`, { borderRadius: 50 }]}>
                     <View style={[tw`border-2 overflow-hidden p-4 bg-blue-100 border-white`, { borderRadius: 50 }]}>
                         <AntDesign name="plus" size={24} color="#1E3A8A" />
@@ -20,5 +20,3 @@ const Moments = () => {
 }
 
 export default Moments
-
-const styles = StyleSheet.create({})
